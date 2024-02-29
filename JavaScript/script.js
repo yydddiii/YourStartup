b3587dfb062d93c2dee5c2e11e7d559eda3472f0
yydddiii <154961348+yydddiii@users.noreply.github.com>
2024-02-29 20:54:06 +0300

    side-menu

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -24,3 +24,72 @@ window.addEventListener('scroll', function() {
     header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
   }
 });
+
+document.querySelector('.application__select-arrow').addEventListener('click', function(){
+  this.classList.toggle('open');
+  var submenu_wrapper = document.querySelector('.application__submenu-rate-wrapper');
+  if(submenu_wrapper.style.visibility === 'hidden'){
+    submenu_wrapper.style.visibility = "visible";
+    submenu_wrapper.style.opacity = 1;
+  }else{
+    submenu_wrapper.style.visibility = "hidden";
+    submenu_wrapper.style.opacity = 0;
+  }
+  document.querySelector('.application__select-arrow').classList.toggle('arrow-active');
+});
+
+document.querySelector('.support__button').addEventListener('click', function(){
+  document.querySelector('.application').classList.toggle('called')
+  document.querySelector('.header').classList.toggle('hide')
+});
+
+document.querySelector('.right-menu__button').addEventListener('click', function(){
+  document.querySelector('.application').classList.toggle('called')
+  document.querySelector('.header').classList.toggle('hide')
+});
+
+document.querySelector('.big-button__button').addEventListener('click', function(){
+  document.querySelector('.application').classList.toggle('called')
+  document.querySelector('.header').classList.toggle('hide')
+});
+
+document.querySelector('.application__cross-img').addEventListener('click', function(){
+  document.querySelector('.application').classList.toggle('called')
+  document.querySelector('.header').classList.toggle('hide')
+});
+
+document.querySelector('.application__submenu-option1').addEventListener('click', function(){
+  var submenu_wrapper = document.querySelector('.application__submenu-rate-wrapper');
+  document.querySelector('.application__select-arrow').classList.toggle('arrow-active')
+  if(submenu_wrapper.style.visibility === 'hidden'){
+    submenu_wrapper.style.visibility = "visible";
+    submenu_wrapper.style.opacity = 1;
+  }else{
+    submenu_wrapper.style.visibility = "hidden";
+    submenu_wrapper.style.opacity = 0;
+  }
+});
+
+document.querySelector('.application__submenu-option2').addEventListener('click', function(){
+  var submenu_wrapper = document.querySelector('.application__submenu-rate-wrapper');
+  document.querySelector('.application__select-arrow').classList.toggle('arrow-active')
+  if(submenu_wrapper.style.visibility === 'hidden'){
+    submenu_wrapper.style.visibility = "visible";
+    submenu_wrapper.style.opacity = 1;
+  }else{
+    submenu_wrapper.style.visibility = "hidden";
+    submenu_wrapper.style.opacity = 0;
+  }
+});
+
+document.querySelector('.application__submenu-option3').addEventListener('click', function(){
+  var submenu_wrapper = document.querySelector('.application__submenu-rate-wrapper');
+  document.querySelector('.application__select-arrow').classList.toggle('arrow-active')
+  if(submenu_wrapper.style.visibility === 'hidden'){
+    submenu_wrapper.style.visibility = "visible";
+    submenu_wrapper.style.opacity = 1;
+  }else{
+    submenu_wrapper.style.visibility = "hidden";
+    submenu_wrapper.style.opacity = 0;
+  }
+});
