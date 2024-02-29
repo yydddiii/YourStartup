6fcceffea733397f72eb6e656e8c514ccf081d6e
yydddiii <154961348+yydddiii@users.noreply.github.com>
2024-02-29 12:35:36 +0300

    full-adoptation

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,26 @@
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('.header');
-    if (window.scrollY > 0) {
-      header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-    } else {
-      header.style.backgroundColor = 'transparent';
-    }
-  });
+var count = 0
 
 document.querySelector('.burger-menu').addEventListener('click', function(){
+  var header = document.querySelector('.header');
   this.classList.toggle('active');
-  document.querySelector('.menu').classList.toggle('open');
-  document.querySelector('.right-menu').classList.toggle('open');
+  document.querySelector('.menu').classList.toggle('menu_visible');
+  document.querySelector('.right-menu').classList.toggle('menu_visible');
+  if(count === 0){
+    count = 1
+    header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  }else if(window.scrollY > 0){
+    count = 0
+  }else{
+  count = 0
+  header.style.backgroundColor = 'transparent';
+}
 })
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.header');
+  if(window.scrollY === 0 && count === 0){
+    header.style.backgroundColor = 'transparent';
+  }else if (window.scrollY > 0) {
+    header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  }
+});
