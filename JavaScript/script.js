3de7303564afff7b8ecc6c5190d70cc4acefa217
yydddiii <154961348+yydddiii@users.noreply.github.com>
2024-02-29 03:28:30 +0300

    burger-menu

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,3 +6,8 @@ window.addEventListener('scroll', function() {
       header.style.backgroundColor = 'transparent';
     }
   });
+
+document.querySelector('.burger-menu').addEventListener('click', function(){
+  this.classList.toggle('active');
+  document.querySelector('.menu').classList.toggle('open');
+})
